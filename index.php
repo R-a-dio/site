f5c1dfc3ecc2c8ef556c3fed5ab2ac3b68dc56ec
David Collinson <hiroto@hiroto.eu>
2013-08-30 07:29:42 +0100

    added a real index file.

--- index.php
+++ index.php
@@ -1,17 +1,54 @@
 <?php
 
 
-	if ($_ENV["RADIO_SERVER_CONFIG"])
-		require("config/" . $_ENV["RADIO_SERVER_CONFIG"] . ".php");
-	else
-		require("config/development.php") or die("config can't be found! (or read)");
+    if ($_ENV["RADIO_SERVER_CONFIG"])
+        require("config/" . $_ENV["RADIO_SERVER_CONFIG"] . ".php");
+    else
+        require("config/development.php") or die("config can't be found! (or read)");
+
+
+    require("functions/db.php");
+    require("functions/core.php");
+
+    require("functions/news.php");
+    require("functions/stats.php");
+    require("functions/graph.php");
+    require("functions/user.php");
+    require("functions/routes.php");
 
 
 
-	require("functions/core.php");
+    $query_string = $_SERVER["QUERY_STRING"];
 
-	require("functions/news.php");
-	require("functions/stats.php");
-	require("functions/graph.php");
-	require("functions/user.php");
-	require("functions/routes.php");
+
+    $route = find_route($query_string);
+
+
+    if ( ! $route ) {
+        redirect("404");
+    } else {
+        ob_start();
+
+        echo "<!DOCTYPE html>";
+
+        echo "<html>";
+
+            require_once("templates/head.php");
+
+            echo "<body>";
+
+                require_once("templates/navbar.php")
+
+                require_once($route);
+
+                if ($route != $home)
+                    require_once("templates/footer.php")
+
+                require_once("templates/postscript.php");
+
+            echo "</body>";
+
+        echo "</html>";
+
+        ob_flush();
+    }

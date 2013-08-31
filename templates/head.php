63ac61793fb8d5412faef025f74122cd3bceb4fe
David Collinson <hiroto@hiroto.eu>
2013-08-31 16:17:28 +0100

    spaces -> tabs

--- templates/head.php
+++ templates/head.php
@@ -1,19 +1,19 @@
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta name="description" content="">
-        <meta name="author" content="">
-        <link rel="shortcut icon" href="/favicon.ico">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta name="description" content="">
+		<meta name="author" content="">
+		<link rel="shortcut icon" href="/favicon.ico">
 
-        <title>R/a/dio</title>
+		<title>R/a/dio</title>
 
-        <!-- Bootstrap Core CSS
-            ==================== -->
-        <link href="./css/bootstrap.css" rel="stylesheet">
+		<!-- Bootstrap Core CSS
+			==================== -->
+		<link href="./css/bootstrap.css" rel="stylesheet">
 
 
-        <!-- IE7-9 are not supported in any way by this site.
-            They can all die in a fire. (IE10 + 11, however, live)
-            ======================================================= -->
+		<!-- IE7-9 are not supported in any way by this site.
+			They can all die in a fire. (IE10 + 11, however, live)
+			======================================================= -->
 
-    </head>
+	</head>

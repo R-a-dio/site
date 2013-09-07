ff1795fdf9d52c3fb08a671e6dd316599deb4613
David Collinson <hiroto@hiroto.eu>
2013-09-07 18:52:12 +0100

    ...

--- app/controllers/BaseController.php
+++ app/controllers/BaseController.php
@@ -21,7 +21,7 @@ class BaseController extends Controller {
 			// TODO: dynamic source for the themes
 			$this->layout = View::make($this->layout)
 				->with("base", Config::get("app.base", "/"))
-				->with("theme", $this->getTheme()));
+				->with("theme", $this->getTheme());
 		}
 	}
 

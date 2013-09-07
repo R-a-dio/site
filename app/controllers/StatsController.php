3c311bbb4533fbf47d4289fd5265097045dfdbfd
David Collinson <hiroto@hiroto.eu>
2013-09-07 23:46:21 +0100

    updated news, added controller interface

---
+++ app/controllers/StatsController.php
@@ -0,0 +1,33 @@
+<?php
+
+class StatsController extends BaseController {
+	protected $layout = 'master';
+
+
+	private function getFaveStats() {
+
+	}
+
+	private function getDJStats() {
+
+	}
+
+	private function getGraphs() {
+
+	}
+
+
+
+	public function showGraphs() {
+
+	}
+
+	public function showDJStats() {
+
+	}
+
+	public function showFaveStats() {
+
+	}
+
+}

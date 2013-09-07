709eec08fa51b00731233373ff483db66b030eba
David Collinson <hiroto@hiroto.eu>
2013-09-07 19:56:38 +0100

    added base URL to navbar links

--- app/views/layouts/navbar.blade.php
+++ app/views/layouts/navbar.blade.php
@@ -11,35 +11,35 @@
 						<span class="icon-bar"></span>
 						<span class="icon-bar"></span>
 					</button>
-					<a class="navbar-brand" href="#/home"><img src="./assets/logotitle_2.png" height="22"></a>
+					<a class="navbar-brand" href="{{ $base }}"><img src="{{ $base }}/assets/logotitle_2.png" height="22"></a>
 				</div>
 
 				<!-- Navbar Itself
 				======== -->
 				<div class="collapse navbar-collapse">
 					<ul class="nav navbar-nav">
-						<li class="active"><a href="#">Home</a></li>
-						<li><a href="/news">News</a></li>
+						<li class="active"><a href="{{ $base }}">Home</a></li>
+						<li><a href="{{ $base }}/news">News</a></li>
 
 						<li class="dropdown">
 							<a href="#" class="dropdown-toggle" data-toggle="dropdown">Play Data <b class="caret"></b></a>
 							<ul class="dropdown-menu">
-								<li><a href="#/last-played">Last Played</a></li>
-								<li><a href="#/queue">Queue</a></li>
-								<li><a href="#/faves">Favourites</a></li>
+								<li><a href="{{ $base }}/last-played">Last Played</a></li>
+								<li><a href="{{ $base }}/queue">Queue</a></li>
+								<li><a href="{{ $base }}/faves">Favourites</a></li>
 							</ul>
 						</li>
 
 						<li class="dropdown">
 							<a href="#" class="dropdown-toggle" data-toggle="dropdown">Stats <b class="caret"></b></a>
 							<ul class="dropdown-menu">
-								<li><a href="stats/graphs">Graphs &amp; Metrics</a></li>
-								<li><a href="stats/djs">Staff</a></li>
-								<li><a href="stats/faves">Favourites</a></li>
+								<li><a href="{{ $base }}/stats/graphs">Graphs &amp; Metrics</a></li>
+								<li><a href="{{ $base }}/stats/djs">Staff</a></li>
+								<li><a href="{{ $base }}/stats/faves">Favourites</a></li>
 							</ul>
 						</li>
 
-						<li><a href="#/submit">Submit Songs</a></li>
+						<li><a href="{{ $base }}/submit">Submit Songs</a></li>
 					</ul>
 					<form class="navbar-form navbar-right">
 						<div class="form-group">

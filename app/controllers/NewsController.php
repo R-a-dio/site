f67d828ec089a7f21962b200c74f2942d0a91f7c
Amelia Ikeda <hiroto@hiroto.eu>
2013-09-09 18:16:23 +0100

    typo.

--- app/controllers/NewsController.php
+++ app/controllers/NewsController.php
@@ -50,7 +50,7 @@ class NewsController extends BaseController {
 						</a>
 					  </h4>
 					</div>
-					<div id="collapse-news-{$result["id"]} class="panel-collapse collapse">
+					<div id="collapse-news-{$result["id"]}" class="panel-collapse collapse">
 						<div class="panel-body">
 							<span class="date">{$result["time"]}</span>
 							{$result["newstext"]}

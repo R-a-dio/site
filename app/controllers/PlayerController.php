a0de8c8b52fa254ea56f17f2d4606ee377635001
Amelia Ikeda <hiroto@hiroto.eu>
2013-09-13 20:50:33 +0100

    added playercontroller

---
+++ app/controllers/PlayerController.php
@@ -0,0 +1,47 @@
+<?php
+
+class PlayerController extends BaseController {
+
+
+	/*
+	|--------------------------------------------------------------------------
+	| Player (faves|queue) Controller
+	|--------------------------------------------------------------------------
+	|
+	| Generates current song info for the player and the homepage
+	| (with an interface for the last played and queue pages)
+	| 
+	| This shouldnt directly route to anything.
+	|
+	*/
+
+	/**
+	 * Retrieve the current queue.
+	 * 
+	 * @param int $limit
+	 *
+	 * @return array
+	 */
+	protected function getQueueArray($limit = 5) {
+		return DB::table('curqueue')
+			->take($limit)
+			->get();	
+	}
+
+	protected function getLastPlayedArray($limit = 5) {
+
+		// this query is utter shit and you should feel bad if you made this.
+		return DB::table('eplay')
+			->select(
+				'eplay.dt as lastplayed',
+				'esong.meta as metadata',
+				DB::raw('(select count(*) from efave where eplay.isong = esong.isong) as faves'),
+				DB::raw('(select count(*) from eplay where eplay.isong = esong.id) as plays'),
+			)
+			->join('esong', 'eplay.isong', "=", 'esong.id')
+			->orderBy('eplay.dt', 'desc')
+			->take($limit)
+			->get();
+	}
+
+}

38f920d79c5a3663b6083887130ae85f70239ba1
Amelia Ikeda <hiroto@hiroto.eu>
2013-10-01 23:49:32 +0100

    change navbar styling [ci skip]

--- app/views/admin/navbar.blade.php
+++ app/views/admin/navbar.blade.php
@@ -33,14 +33,24 @@
 						<li><a href="{{ $base }}/admin/djs">DJs</a><li>
 					</ul>
 					<ul class="nav navbar-nav navbar-right">
-						<li><a href="#">Hiroto <span class="badge">42</span></a></li>
 						<li class="dropdown">
-						<a href="#" class="dropdown-toggle" data-toggle="dropdown">My Profile <b class="caret"></b></a>
+							<a href="#" class="dropdown-toggle" data-toggle="dropdown">Hiroto <span class="badge">42</span></a>
 						<ul class="dropdown-menu">
-							<li><a href="#">Edit</a></li>
-							<li><a href="#">DJ Profile</a></li>
-							<li><a href="#">Change Password</a></li>
-							<li><a href="#">Log Out</a></li>
+							<li><a href="{{ $base }}/admin/notifications">Site's broken again</a></li>
+							<li><a href="{{ $base }}/admin/notifications">Someone called Yuru Yuri bad again</a></li>
+							<li><a href="{{ $base }}/admin/pending">150 Pending Songs</a></li>
+							<li><a href="{{ $base }}/admin/errors">15 New Errors</a></li>
+							<li><a href="{{ $base }}/admin/users">2 New Users Registered</a></li>
+							<li><a href="{{ $base }}/admin/notifications">37 More...</a></li>
+						</ul>
+						</li>
+						<li class="dropdown">
+						<a href="#" class="dropdown-toggle" data-toggle="dropdown">Profile <b class="caret"></b></a>
+						<ul class="dropdown-menu">
+							<li><a href="{{ $base }}/admin/edit-profile">Edit</a></li>
+							<li><a href="{{ $base }}/admin/dj-profile">DJ Profile</a></li>
+							<li><a href="{{ $base }}/admin/change-password">Change Password</a></li>
+							<li><a href="{{ $base }}/admin/logout">Log Out</a></li>
 						</ul>
 						</li>
 					</ul>

d6d6a28982ace1a6e8c3613c62b06238520234f0
David Collinson <hiroto@hiroto.eu>
2013-10-15 20:48:01 +0100

    Add further style changes to the IRC display

--- app/views/default/irc.blade.php
+++ app/views/default/irc.blade.php
@@ -1,5 +1,7 @@
 @section('content')
-	<iframe src="https://kiwiirc.com/client/irc.rizon.net/#r/a/dio"
-		style="border:0; width:100%; height:700px; margin: 60px 5px 0 5px;"
+<div class="container main">
+	<iframe src="https://kiwiirc.com/client/irc.rizon.net/?theme=cli#r/a/dio"
+		style="border:0; width:100%; height:700px;"
 	></iframe>
+</div>
 @stop

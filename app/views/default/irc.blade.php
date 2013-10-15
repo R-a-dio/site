6a678cf6f59bb9bb1d132e17787af6a4e6c7d6d7
David Collinson <hiroto@hiroto.eu>
2013-10-15 20:37:05 +0100

    Small style fixes for the IRC client on the site

--- app/views/default/irc.blade.php
+++ app/views/default/irc.blade.php
@@ -1,5 +1,5 @@
 @section('content')
 	<iframe src="https://kiwiirc.com/client/irc.rizon.net/#r/a/dio"
-		style="border:0; width:100%; height:700px; margin-top: 60px;"
+		style="border:0; width:100%; height:700px; margin: 60px 5px 0 5px;"
 	></iframe>
 @stop

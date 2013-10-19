c2a020ded422d2648a1eaac0e0c335cd5012dbbd
David Collinson <hiroto@hiroto.eu>
2013-10-19 19:38:10 +0100

    Start caching everything in sight.

--- app/controllers/Search.php
+++ app/controllers/Search.php
@@ -6,39 +6,48 @@ class Search extends Player {
 
 	protected function getSearchResults($search) {
 		if ($search) {
-			$results = DB::table('tracks')
-				->where('usable', '=', 1)
-				->where('need_reupload', '=', 0)
-				->whereRaw('match (artist, track, album, tags) against (?)', [$search])
-				->select("id", "track", "artist", "album", "tags", "lastplayed", "lastrequested", "requestcount", "priority")
-				->get();
+			if (Cache::section('search')->has($search)) {
 
-			$count = 0;
+				return Cache::section('search')->get($search);
 
-			foreach ($results as &$result) {
-				
+			} else {
 
-				$result["break"] = $count;
-				if ($count == 2)
-					$count = 0;
-				else
-					$count++;
+				$results = DB::table('tracks')
+					->where('usable', '=', 1)
+					->where('need_reupload', '=', 0)
+					->whereRaw('match (artist, track, album, tags) against (?)', [$search])
+					->select("id", "track", "artist", "album", "tags", "lastplayed", "lastrequested", "requestcount", "priority")
+					->get();
 
-				$delay = $this->getSongDelay((int) $result["requestcount"]);
+				$count = 0;
 
-				if ((time() - strtotime($result["lastrequested"])) > $delay)
-					$result["cooldown"] = false;
-				else
-					$result["cooldown"] = true;
+				foreach ($results as &$result) {
+					
 
-				// lets us ->format() the resulting time
-				$result["lastplayed"] = new DateTime($result["lastplayed"]);
-				$result["lastrequested"] = new DateTime($result["lastrequested"]);
+					$result["break"] = $count;
+					if ($count == 2)
+						$count = 0;
+					else
+						$count++;
+
+					$delay = $this->getSongDelay((int) $result["requestcount"]);
+
+					if ((time() - strtotime($result["lastrequested"])) > $delay)
+						$result["cooldown"] = false;
+					else
+						$result["cooldown"] = true;
+
+					// lets us ->format() the resulting time
+					$result["lastplayed"] = new DateTime($result["lastplayed"]);
+					$result["lastrequested"] = new DateTime($result["lastrequested"]);
+				}
+				Cache::section('search')->put($search, $results, Config::get('cache.times.search'));
+				return $results;
 			}
-			return $results;
 		} else {
 			return [];
 		}
+
 	}
 
 	public function showResults($search = false) {

0a5e060890b490ed4d359b1cd23fa61528bd670d
David Collinson <hiroto@hiroto.eu>
2013-10-22 21:17:06 +0100

    Add Google+ sign-in button

--- app/views/default/login.blade.php
+++ app/views/default/login.blade.php
@@ -1,5 +1,31 @@
 @section('content')
 
+<div class="container main">
 
+	<div class="container">
+		<div class="col-md-6">
+			<div class="well">
+				<h2 class="text-center text-success">
+					Log In With
+				</h2>
+				{{ Form::open(['url' => 'login']) }}
+					<div class="form-group">
+						<a href="" class="btn btn-google"></a>
+					</div>
+				{{ Form::close() }}
+			</div>
+		</div>
+		<div class="col-md-6">
+			<div class="well">
+				<h2 class="text-center text-warning">
+					Register
+				</h2>
+ 
+			</div>
+		</div>
+	</div>
+
+
+</div>
 
 @stop

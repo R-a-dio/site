4678fc55891624db5f76733c89840c90dff06366
Z.G <zorroguevara@yahoo.fr>
2013-11-07 13:19:32 +0000

    Add initial french translation

---
+++ app/lang/fr/navbar.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+	"home" => "R/a/dio",
+	"news" => "News",
+	"data" => "Play Data",
+	"lp" => "Derniers titres diffusés",
+	"queue" => "File d’attente",
+	"faves" => "Favoris",
+	"stats" => "Statistiques",
+	"submit" => "Envoie",
+	"irc" => "IRC",
+	"search" => "Recherche",
+	"request" => "Requête",
+	"graphs" => "Graphiques",
+	"staff" => "Equipe"
+];
---
+++ app/lang/fr/time.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+	"minute" => ":t minute|:t minutes",
+	"hour" => ":t heure|:t heures",
+	"day" => ":t jour|:t jours",
+	"week" => ":t semaine|:t semaines",
+	"timeago" => "il y a :time",
+	"timeuntil" => "jusqu'à :time"
+]

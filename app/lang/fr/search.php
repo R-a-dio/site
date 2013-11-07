b7ace1aac6f6c2abc67bf1c0909eaca1b273d834
Z.G <zorroguevara@yahoo.fr>
2013-11-07 13:19:32 +0000

    Add initial french translation

---
+++ app/lang/fr/search.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+	"title" => "Recherche",
+	"help" => [
+		"main" => "Rechercher une chanson.",
+		"options" => "Cliquer sur une chanson pour afficher les options",
+	],
+	"placeholder" => "Recherche",
+	"button" => "Recherche",
+	"plays" => "Nombre de fois joués",
+	"faves" => "Favoris",
+	"request" => "Requête",
+	"requestable" => "Requestable",
+	"popover" => [
+		"header" => "Options de la chanson",
+		"lp" => "Dernière fois joué :timeago",
+		"login" => ":login pour l’ajouter en favoris ou la télécharger",
+	],
+
+];
---
+++ app/lang/fr/stream.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+	"play" => "Play",
+	"stop" => "Stop",
+	"loading" => "Chargement...",
+	"options" => "Plus d’options",
+	"links" => [
+	"direct" => "Lien direct du stream",
+		"m3u" => "Playlist (.m3u)",
+		"pls" => " Playlist (.pls)",
+		"help" => "Aide"
+	],
+	"dj" => "DJ",
+	"listeners" => "Auditeurs",
+	"lp" => "Last Played",
+	"queue" => "Queue",
+
+];

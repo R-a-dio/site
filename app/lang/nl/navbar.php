d8a8b4fbb74dddd84d27386992261ff6ec61a321
Harrstein <Harrstein@gmail.com>
2013-11-07 01:15:40 +0100

    Added Dutch translation

---
+++ app/lang/nl/navbar.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+	"home" => "R/a/dio",
+	"news" => "Nieuws",
+	"data" => "Afspeel Data",
+	"lp" => "Recent Afgespeeld",
+	"queue" => "Wachtrij",
+	"faves" => "Favorieten",
+	"stats" => "Statistieken",
+	"submit" => "Voeg Toe",
+	"irc" => "IRC",
+	"search" => "Zoek",
+	"request" => "Verzoek",
+	"graphs" => "Grafieken",
+	"staff" => "Personeel"
+
+];

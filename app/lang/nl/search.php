3f934c0a60277489c00f897b7e79dbc4c1bbac1d
Harrstein <Harrstein@gmail.com>
2013-11-07 01:15:40 +0100

    Added Dutch translation

---
+++ app/lang/nl/search.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+	"title" => "Zoek",
+	"help" => [
+		"main" => "Zoek een nummer.",
+		"options" => "Klik op een nummer voor opties",
+	],
+	"placeholder" => "Zoek",
+	"button" => "Zoek",
+	"plays" => "Malen afgespeeld",
+	"faves" => "Favorieten",
+	"request" => "Verzoek",
+	"requestable" => "Verzoekbaar",
+	"popover" => [
+		"header" => "Nummer Opties",
+		"lp" => "Laatst Afgespeeld :timeago",
+		"login" => ":login om aan favorieten toe te voegen of te downloaden",
+	],
+
+];
---
+++ app/lang/nl/time.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+	"minute" => ":t minuut|:t minuten",
+	"hour" => ":t uur|:t uren",
+	"day" => ":t dag|:t dagen",
+	"week" => ":t week|:t weken ",
+	"timeago" => ": Geleden",
+	"timeuntil" => "Over :time"
+]

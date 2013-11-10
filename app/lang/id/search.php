a8c150cde95d994369b0638eda74c9c1f424a064
s4nji <sanjitok@yahoo.com>
2013-11-10 13:36:45 +0100

    Indonesian Translation

---
+++ app/lang/id/search.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+    "title" => "Pencarian",
+    "help" => [
+        "main" => "Cari sebuah lagu.",
+        "options" => "Klik pada lagu untuk pilihan",
+    ],
+    "placeholder" => "Cari",
+    "button" => "Cari",
+    "plays" => "Jumlah Dimainkan",
+    "faves" => "Jumlah Favorit",
+    "request" => "Pesan Lagu",
+    "requestable" => "Dapat dipesan",
+    "popover" => [
+        "header" => "Opsi Lagu",
+        "lp" => "Terakhir kali dimainkan :timeago",
+        "login" => ":login to fave or download",
+    ],
+
+];
---
+++ app/lang/id/time.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    "minute" => ":t menit",
+    "hour" => ":t jam",
+    "day" => ":t hari",
+    "week" => ":t minggu",
+    "timeago" => ":time yang lalu",
+    "timeuntil" => "dalam :time"
+]

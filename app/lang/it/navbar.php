415bd52b79e6fe61bfa7b17bac37558b8fcd7218
zukivi <zukivi93@gmail.com>
2013-11-19 22:33:00 +0100

    Site translation in italian.

---
+++ app/lang/it/navbar.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+	"home" => "R/a/dio",
+	"news" => "Notizie",
+	"data" => "Inizia data",
+	"lp" => "Ultime canzoni",
+	"queue" => "In coda",
+	"faves" => "Favoriti",
+	"stats" => "Statistiche",
+	"submit" => "Invia",
+	"irc" => "IRC",
+	"search" => "Cerca",
+	"request" => "Richiedi",
+	"graphs" => "Grafici & Metriche",
+	"staff" => "Staff"
+];

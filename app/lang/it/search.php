523ebd8b7c6e3e50aa2b217bdad38903dc9e90ca
zukivi <zukivi93@gmail.com>
2013-11-19 22:33:00 +0100

    Site translation in italian.

---
+++ app/lang/it/search.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+	"title" => "Cerca",
+	"help" => [
+		"main" => "Cerca per una canzone.",
+		"options" => "Clicca su una canzone per le opzioni",
+	],
+	"placeholder" => "Cerca",
+	"button" => "Cerca",
+	"plays" => "Numero di volte suonata",
+	"faves" => "Favoriti",
+	"request" => "Richieste",
+	"requestable" => "Richiedibile",
+	"popover" => [
+		"header" => "Opzioni canzone",
+		"lp" => "Suonata ultima volta: :timeago",
+		"login" => ":login per scaricare o mettere in preferiti",
+	],
+
+];
---
+++ app/lang/it/time.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+	"minute" => ":t minuto|:t minuti",
+	"hour" => ":t ora|:t ore",
+	"day" => ":t giorno|:t giorni",
+	"week" => ":t settimana|:t settimane",
+	"timeago" => ":time fa",
+	"timeuntil" => "fra :time"
+]

5baf18f78b2bb20c731bad711b89fe8e39b1bfbe
Erik Andrés Regla Torres <jvarred@gmail.com>
2014-01-15 03:11:58 -0300

    Site translation in spanish

---
+++ app/lang/spa/reminders.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+	/*
+	|--------------------------------------------------------------------------
+	| Password Reminder Language Lines
+	|--------------------------------------------------------------------------
+	|
+	| The following language lines are the default lines which match reasons
+	| that are given by the password broker for a password update attempt
+	| has failed, such as for an invalid token or invalid new password.
+	|
+	*/
+
+	"password" => "Las contraseñas deben ser de seis caracteres y deben coincidir para su confirmación.",
+
+	"user"     => "No podemos encontrar un usuario con esa dirección de e-mail.",
+
+	"token"    => "Esta llave de reestablecimiento de contraseña es inválida.",
+
+];

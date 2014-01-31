cc06c7a1468cdaa76ad504ac667935ded0aa97bf
David Collinson <hiroto@hiroto.eu>
2014-01-31 12:43:04 +0000

    Remove 404'd pages from the navbar

--- app/views/layouts/navbar.blade.php
+++ app/views/layouts/navbar.blade.php
@@ -19,25 +19,6 @@
 				<div class="collapse navbar-collapse">
 					<ul class="nav navbar-nav">
 						<li><a href="/news" class="ajax-navigation">{{{ trans("navbar.news") }}}</a></li>
-
-						<li class="dropdown">
-							<a href="#" class="dropdown-toggle" data-toggle="dropdown">{{{ trans("navbar.data") }}} <b class="caret"></b></a>
-							<ul class="dropdown-menu">
-								<li><a href="/last-played" class="ajax-navigation">{{{ trans("navbar.lp") }}}</a></li>
-								<li><a href="/queue" class="ajax-navigation">{{{ trans("navbar.queue") }}}</a></li>
-								<li><a href="/faves" class="ajax-navigation">{{{ trans("navbar.faves") }}}</a></li>
-							</ul>
-						</li>
-
-						<li class="dropdown">
-							<a href="#" class="dropdown-toggle" data-toggle="dropdown">{{{ trans("navbar.stats") }}} <b class="caret"></b></a>
-							<ul class="dropdown-menu">
-								<li><a href="/stats" class="ajax-navigation">{{{ trans("navbar.graphs") }}}</a></li>
-								<li><a href="/staff" class="ajax-navigation">{{{ trans("navbar.staff") }}}</a></li>
-							</ul>
-						</li>
-
-						<li><a href="/submit" class="ajax-navigation">{{{ trans("navbar.submit") }}}</a></li>
 						<li><a href="/irc" class="ajax-navigation">{{{ trans("navbar.irc") }}}</a></li>
 					</ul>
 					{{ Form::open(['url' => "/search", "class" => "navbar-form navbar-right" ]) }}
@@ -46,7 +27,7 @@
 						</div>
 					{{ Form::close() }}
 				</div><!--/.nav-collapse -->
-				<audio src="https://r-a-d.io/main" preload="metadata" id="player"></audio>
+				<div id="stream"></div>
 			</div>
 
 			

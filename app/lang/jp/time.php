e2c5fff92b2d4503f412f1481239bf9d1d07afff
Dylan Fawkes <landyl123@gmail.com>
2014-02-02 05:33:21 +0100

    Add japanese locale

---
+++ app/lang/jp/time.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+	"minute" => ":t minute|:t minutes",
+	"hour" => ":t hour|:t hours",
+	"day" => ":t day|:t days",
+	"week" => ":t week|:t weeks",
+	"timeago" => ":time ago",
+	"timeuntil" => "in :time"
+]

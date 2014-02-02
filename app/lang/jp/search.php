f7f0a369a0ee1aaf2fa2de7c7b9f63764188dc86
Dylan Fawkes <landyl123@gmail.com>
2014-02-02 05:33:21 +0100

    Add japanese locale

---
+++ app/lang/jp/search.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+	"title" => "検索",
+	"help" => [
+		"main" => "曲を検索する",
+		"options" => "オプションは曲をクリックしてください",
+	],
+	"placeholder" => "検索",
+	"button" => "検索",
+	"plays" => "放送回数",
+	"faves" => "好み数",
+	"request" => "リクエスト",
+	"requestable" => "リクエストＯＫ",
+	"popover" => [
+		"header" => "オプション",
+		"lp" => "先放送 :timeago 前",
+		"login" => "ダウンロードや好み機能を使うように :login ",
+	],
+
+];

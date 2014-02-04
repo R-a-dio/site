fb15cf2842e6c6a2de741bf10fd3e8282bb9dc58
David Collinson <hiroto@hiroto.eu>
2014-02-04 01:01:55 +0000

    Update active classes on the navbar when navigating pages

--- app/views/layouts/navbar.blade.php
+++ app/views/layouts/navbar.blade.php
@@ -18,11 +18,48 @@
 				======== -->
 				<div class="collapse navbar-collapse">
 					<ul class="nav navbar-nav">
-						<li><a href="/news" class="ajax-navigation">{{{ trans("navbar.news") }}}</a></li>
-						<li><a href="/irc" class="ajax-navigation">{{{ trans("navbar.irc") }}}</a></li>
-						<li><a href="/search" class="ajax-navigation">{{{ trans("search.title") }}}</a></li>
-						@if (Auth::check() and Auth::user()->canDoPending())
-							<li><a href="/admin" target="_blank"><i class="fa fa-star"></i></a></li>
+
+
+						@if (Request::segment(1) == "news")
+							<li class="active">
+						@else
+							<li>
+						@endif
+							<a href="/news" class="ajax-navigation">{{{ trans("navbar.news") }}}</a>
+						</li>
+
+
+						@if (Request::segment(1) == "irc")
+							<li class="active">
+						@else
+							<li>
+						@endif
+							<a href="/irc" class="ajax-navigation">{{{ trans("navbar.irc") }}}</a>
+						</li>
+
+
+						@if (Request::segment(1) == "search")
+							<li class="active">
+						@else
+							<li>
+						@endif
+							<a href="/search" class="ajax-navigation">{{{ trans("search.title") }}}</a>
+						</li>
+
+						@if (Request::segment(1) == "queue" or Request::segment(1) == "last-played")
+							<li class="dropdown active">
+						@else
+							<li class="dropdown">
+						@endif
+							<a href="#" class="dropdown-toggle" data-toggle="dropdown">Stats <b class="caret"></b></a>
+							<ul class="dropdown-menu">
+								<li><a href="/last-played" class="ajax-navigation drop">Last Played</a></li>
+								<li><a href="/queue" class="ajax-navigation drop">Queue</a></li>
+							</ul>
+						</li>
+
+						@if (Auth::user() and Auth::user()->canDoPending())
+							<li><a href="/admin"><i class="fa fa-star"></i></a></li>
 						@endif
 					</ul>
 					{{ Form::open(['url' => "/search", "class" => "ajax-search navbar-form navbar-right" ]) }}

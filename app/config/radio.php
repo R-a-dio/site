aa2a1cf1a7cc8d447d2994bb5e105f792a79da19
David Collinson <hiroto@hiroto.eu>
2014-02-09 23:53:29 +0000

    Change default paths for config/radio.php to work with production

--- app/config/radio.php
+++ app/config/radio.php
@@ -5,7 +5,7 @@ return [
 		"limit" => 25,
 	],
 	"paths" => [
-		"pending" => app_path() . "/storage/songs/pending/",
-		"music" => app_path() . "/storage/songs/",
+		"pending" => "/radio/music/pending",
+		"music" => "/radio/music",
 	],
 ];

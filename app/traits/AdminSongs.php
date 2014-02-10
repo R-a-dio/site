855f82b90a664d1ebe170fedb72c0e3e5b7d7725
David Collinson <hiroto@hiroto.eu>
2014-02-10 18:25:36 +0000

    Add handling for POSTs to the pending songs page

--- app/traits/AdminSongs.php
+++ app/traits/AdminSongs.php
@@ -3,11 +3,97 @@
 trait AdminSongs {
 
 	public function getPending() {
-		$this->layout->content = View::make("admin.pending");
+		$pending = DB::table("pending")
+			->orderBy("id", "desc")
+			->get();
+		$this->layout->content = View::make("admin.pending")
+			->with("pending", $pending);
 	}
 
 	public function postPending($id) {
+		$pending = DB::table("pending")
+			->where("id", "=", $id)
+			->first();
 
+		$delete = false;
+
+		$action = Input::get("action");
+
+		if ($pending) {
+			switch ($action) {
+				case "delete":
+					$reason = Input::get("reason", "");
+					$meta = $pending["track"] ?: $pending["origname"];
+					DB::table("postpending")
+						->insert([
+							"reason" => $reason,
+							"ip" => $pending["submitter"],
+							"accepted" => 0,
+							"meta" => $meta,
+						]);
+					unlink(Config::get("radio.paths.pending") . $pending["path"]);
+					$delete = true;
+					break;
+				case "replace":
+					$replace = Input::get("replace");
+					$check = DB::table("tracks")
+						->where("id", "=", $replace)
+						->first();
+					if ($check) {
+						DB::table("tracks")
+							->where("id", "=", $check["id"])
+							->update([
+								"usable" => 0,
+								"lasteditor" => Auth::user()->user,
+							]);
+						$delete = true;
+						rename(Config::get("radio.paths.pending") . $pending["path"], Config::get("radio.paths.music") . $check["path"]);
+					}
+					
+					break;
+				case "accept":
+					$editor = Auth::user()->user;
+					$artist = Input::get("artist", "");
+					$title = Input::get("title", null);
+					$album = Input::get("album", "");
+					$tags = Input::get("tags", "");
+					$hash = sha1(strtolower(trim($artist != "" ? "$artist - $title" : $title)));
+
+					// title is required.
+					if ($title) {
+						$delete = true;
+						DB::table("tracks")
+							->insert([
+								"track" => $title,
+								"artist" => $artist,
+								"album" => $album,
+								"tags" => $tags,
+								"path" => $pending["path"],
+								"usable" => 0,
+								"accepter" => $editor,
+								"lasteditor" => $editor,
+								"hash" => $hash,
+							]);
+						rename(Config::get("radio.paths.pending") . $pending["path"], Config::get("radio.paths.music") . $pending["path"]);
+					}
+					break;
+				default:
+					break;
+			}
+
+
+			if ($delete) {
+				DB::table("pending")
+					->where("id", "=", $pending["id"])
+					->delete();
+			}
+
+		}
+
+		if (Request::ajax())
+			return Response::json(["status" => !$delete]);
+		else
+			return Response::redirect("/admin/pending");
 	}
 
 }

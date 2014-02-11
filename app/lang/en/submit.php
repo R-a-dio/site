3b61504f0565850d5ea18da2a51b89f2ca707018
Vin <bevin@jabana.se>
2014-02-11 17:35:28 +0000

    Cleaned up submit page text Added localization on submit page

---
+++ app/lang/en/submit.php
@@ -0,0 +1,28 @@
+<?php
+
+return [
+	"title" => "Submit",
+	"guidelines" => [
+		"upload" => [
+			"title" => "Uploading Guidelines",
+			"search" => "Search first to make sure that the song isn't already in the database.",
+			"quality" => "No less than 128 kbps; that means no YouTube rips!",
+			"source" => "Please include the source (anime, VN, etc) in the comment field.",
+		],
+		"tagging" => [
+			"title" => "Tagging Guidelines",
+			"required" => "Both artist and title are required.",
+			"runes" => "Avoid Japanese characters unless it's absolutely necessary.",
+			"cv" => "Do not include character names or \"CV\" notation; vocalists only."
+		]
+	],
+	"uploaddesc" => "Upload a song to the R/a/dio database. Try to keep MP3s to around 15MB.",
+	"comment" => "Comment",
+	"commentdesc" => "Add the source, artist and title if tags are missing, etc.",
+	"daypass" => "Daypass",
+	"daypassdesc" => "Enter the daypass and you can have unlimited uploads until midnight, UTC.",
+	"upload" => "Upload Song",
+	"accepts" => "Accepts",
+	"declines" => "Declines",
+];
+

1c9ebb92088aed3cdd559cf677331ecda564d8d9
David Collinson <hiroto@hiroto.eu>
2014-02-28 05:09:09 +0000

    Update news page to be more user-friendly when unable to edit news posts

--- app/views/admin/news.blade.php
+++ app/views/admin/news.blade.php
@@ -1,7 +1,7 @@
 @section("content")
 
 	<div class="container">
-
+		
 		@if ($id)
 
 			@if ($news["private"])
@@ -27,15 +27,27 @@
 					{{ Form::open(["method" => "PUT"]) }}
 						<div class="form-group">
 							<label>Title</label>
-							<input type="text" name="title" value="{{{ $news->title }}}" class="form-control">
+							<input type="text" name="title" value="{{{ $news->title }}}" class="form-control"
+							@if (!Auth::user()->canPostNews())
+								disabled
+							@endif
+							>
 						</div>
 						<div class="form-group">
 							<label>Header</label>
-							<textarea name="header" id="" cols="30" rows="10" class="form-control">{{{ $news->header }}}</textarea>
+							<textarea name="header" id="" cols="30" rows="10" class="form-control"
+							@if (!Auth::user()->canPostNews())
+								disabled
+							@endif
+							>{{{ $news->header }}}</textarea>
 						</div>
 						<div class="form-group">
 							<label>Body</label>
-							<textarea name="text" id="" cols="30" rows="10" class="form-control">{{{ $news->text }}}</textarea>
+							<textarea name="text" id="" cols="30" rows="10" class="form-control"
+							@if (!Auth::user()->canPostNews())
+								disabled
+							@endif
+							>{{{ $news->text }}}</textarea>
 						</div>
 						<div class="radio">
 							<label>
@@ -57,65 +69,74 @@
 								Private
 							</label>
 						</div>
-						<button type="submit" class="btn btn-info">Update Post</button>
+						<button type="submit" class="btn btn-info"
+						@if (!Auth::user()->canPostNews())
+							disabled
+						@endif
+						>Update Post</button>
 					{{ Form::close() }}
-					<hr>
-					@if (Auth::user()->isAdmin())
-						{{ Form::open(["method" => "DELETE"]) }}
 
-							<button type="submit" class="btn btn-danger">Delete Post</button> &#8212; Danger Zone&trade; (Don't worry, nothing is deleted; it's soft-deleted and can be recovered)
+					@if (Auth::user()->canPostNews())
+						<hr>
+						@if (Auth::user()->isAdmin())
+							{{ Form::open(["method" => "DELETE"]) }}
 
-						{{ Form::close() }}
+								<button type="submit" class="btn btn-danger">Delete Post</button> &#8212; Danger Zone&trade; (Don't worry, nothing is deleted; it's soft-deleted and can be recovered)
+
+							{{ Form::close() }}
+						@endif
 					@endif
 				</div>
 			</div>
 
 		@else
 
-			<div class="panel-group" id="accordion">
+			@if (Auth::user()->canPostNews())
+				<div class="panel-group" id="accordion">
 
-				<div class="panel panel-info">
-					<div class="panel-heading">
-						<h4 class="panel-title">
-							<a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion" href="#collapse-new-post">
-								New Post <span class="pull-right text-muted">Logged in as {{{ Auth::user()->user }}}</span>
-							</a>
-						</h4>
-					</div>
-					<div id="collapse-new-post" class="panel-collapse collapse">
-						<div class="panel-body">
-							{{ Form::open(["method" => "POST"]) }}
-								<div class="form-group">
-									<label>Title</label>
-									<input type="text" name="title" placeholder="Title" class="form-control">
-								</div>
-								<div class="form-group">
-									<label>Header</label>
-									<textarea name="header" id="" cols="30" rows="10" class="form-control" placeholder="Header"></textarea>
-								</div>
-								<div class="form-group">
-									<label>Body</label>
-									<textarea name="text" id="" cols="30" rows="10" class="form-control" placeholder="Body"></textarea>
-								</div>
-								<div class="radio">
-									<label>
-										<input type="radio" name="private" value="0" checked>
-										Public
-									</label>
-								</div>
-								<div class="radio">
-									<label>
-										<input type="radio" name="private" value="1">
-										Private
-									</label>
-								</div>
-								<button type="submit" class="btn btn-info">Create Post</button>
-							{{ Form::close() }}
+					<div class="panel panel-info">
+						<div class="panel-heading">
+							<h4 class="panel-title">
+								<a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion" href="#collapse-new-post">
+									New Post <span class="pull-right text-muted">Logged in as {{{ Auth::user()->user }}}</span>
+								</a>
+							</h4>
 						</div>
-					</div>	
+						<div id="collapse-new-post" class="panel-collapse collapse">
+							<div class="panel-body">
+								{{ Form::open(["method" => "POST"]) }}
+									<div class="form-group">
+										<label>Title</label>
+										<input type="text" name="title" placeholder="Title" class="form-control">
+									</div>
+									<div class="form-group">
+										<label>Header</label>
+										<textarea name="header" id="" cols="30" rows="10" class="form-control" placeholder="Header"></textarea>
+									</div>
+									<div class="form-group">
+										<label>Body</label>
+										<textarea name="text" id="" cols="30" rows="10" class="form-control" placeholder="Body"></textarea>
+									</div>
+									<div class="radio">
+										<label>
+											<input type="radio" name="private" value="0" checked>
+											Public
+										</label>
+									</div>
+									<div class="radio">
+										<label>
+											<input type="radio" name="private" value="1">
+											Private
+										</label>
+									</div>
+									<button type="submit" class="btn btn-info">Create Post</button>
+								{{ Form::close() }}
+							</div>
+						</div>	
+					</div>
+					
 				</div>
-				
-			</div>
+			@endif
 
 			<hr>
 

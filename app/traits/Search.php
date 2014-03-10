b942001eb7acc0e417f62c12d106cb24d234f300
David Collinson <hiroto@hiroto.eu>
2014-03-10 15:10:22 +0000

    Use elasticsearch instead of touching the database when searching.

--- app/traits/Search.php
+++ app/traits/Search.php
@@ -2,35 +2,79 @@
 
 trait Search {
 
-	protected function getSearchResults($request) {
-		if ($request) {
+	protected function getSearchResults($request, $amount = 20) {
 		
-			$results = DB::table('tracks')
-				->where('usable', '=', 1)
-				->where('need_reupload', '=', 0)
-				->whereRaw('match (artist, track, album, tags) against (?)', [$request])
-				->select("id", "track", "artist", "album", "tags", "lastplayed", "lastrequested", "requestcount", "priority")
-				->paginate(15);
+			$results = $this->search($request ?: "", "track", "song-database");
+			//dd($results);
 
-			$search = $results->toArray();
+			$start = (Paginator::getCurrentPage() - 1) * $amount;
 
-			foreach ($search["data"] as &$result) {
+			$count = $results["hits"]["total"];
 
-				$delay = $this->getSongDelay((int) $result["requestcount"]);
-				$lr = strtotime($result["lastrequested"]);
+			$slice = array_slice($results["hits"]["hits"], $start, $amount);
 
-				if (time() - $lr > $delay)
-					$result["cooldown"] = false;
-				else
-					$result["cooldown"] = $lr + $delay;
+			$paginator = Paginator::make($slice, $count, $amount);
 
-			}
+			View::share("time", $results["took"]);
 
-			return ["search" => $search, "links" => $results->links()];
-		} else {
-			return ["search" => false, "links" => ""];
-		}
+			return $paginator;
 
+	}
+
+	protected function search($terms, $type, $index) {
+
+		$params = [
+			"type" => $type,
+			"index" => $index,
+			"ignore" => [
+				400,
+				404
+			],
+			"body" => [
+				"query" => [
+					"query_string" => [
+						"fields" => ["title", "artist", "album", "tags", "_id"],
+						"default_operator" => "and",
+						"query" => $terms,
+					]
+				],
+			],
+		];
+
+		return $this->client->search($params);
+
+	}
+
+	protected function index($track) {
+		$tmp = [];
+
+		$tmp["type"] = "track";
+		$tmp["index"] = "song-database";
+		$tmp["id"] = $track["id"];
+
+		$tmp["body"] = [
+			"album" => $track["album"],
+			"tags" => $track["tags"],
+			"title" => $track["track"],
+			"artist" => $track["artist"],
+			"id" => $track["id"],
+			"acceptor" => $track["accepter"],
+			"editor" => $track["lasteditor"],
+			"requests" => $track["requestcount"],
+			"lastplayed" => $track["lastplayed"],
+			"lastrequested" => $track["lastrequested"],
+			"hash" => $track["hash"],
+			"delay" => $this->getSongDelay($track["requestcount"]),
+		];
+
+		$lr = strtotime($track["lastrequested"]);
+
+		if (time() - $lr > $tmp["body"]["delay"])
+			$tmp["body"]["cooldown"] = false;
+		else
+			$tmp["body"]["cooldown"] = $lr + $tmp["body"]["delay"];
+
+		$this->client->index($tmp);
 	}
 
 }

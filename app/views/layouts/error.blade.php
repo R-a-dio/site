dc6c899525ef8a105b7085a9c48b5d51e74737a9
David Collinson <hiroto@hiroto.eu>
2014-03-10 15:14:13 +0000

    Dump out a reference code when there's an error

--- app/views/layouts/error.blade.php
+++ app/views/layouts/error.blade.php
@@ -3,6 +3,9 @@
 	<div class="container main">
 		<h1 class="text-center text-primary">Whoops! Something broke.</h1>
 		<h2 class="text-center text-danger">{{{ $error }}}</h2>
+		@if (isset($reference))
+			<h3 class="text-center text-success">{{ var_dump($reference) }}</h3>
+		@endif
 	</div>
 
 @stop

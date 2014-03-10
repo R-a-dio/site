1256e3e21a61c27727e6f775d31ea96db5f0ccce
David Collinson <hiroto@hiroto.eu>
2014-03-10 15:22:52 +0000

    Add api localisation

---
+++ app/lang/en/api.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+	"metadata" => [
+		"cv" => "\"CV\" notation is not allowed. " . 
+			"Please change your metadata and remove the parts " .
+			"where Japan has a boner for ignoring the \"Performer\" Field in tags.",
+		"missing" => "You didn't supply any metadata. You should leave a comment with metadata and a source.",
+		"kanji" => "Please provide romaji metadata for this song in a comment.",
+		"success" => "Metadata looks OK",
+	],
+	"upload" => [
+		"cooldown" => "You cannot upload another song just yet. You can upload :time",
+		"no-cooldown" => "You can upload a song!",
+	],
+];

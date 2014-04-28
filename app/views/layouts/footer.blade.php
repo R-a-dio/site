6fd331f1ceb5eddbcdfb317351495caff2e61dcb
David Collinson <hiroto@hiroto.eu>
2014-04-28 23:59:14 +0100

    Misc site design changes based on feedback

--- app/views/layouts/footer.blade.php
+++ app/views/layouts/footer.blade.php
@@ -4,8 +4,6 @@
 					<a class="text-muted" href="/">R/a/dio</a> 
 					|
 					<a class="text-muted" href="https://github.com/R-a-dio/site" target="_blank">Github</a>
-					|
-					<a class="text-muted" href="https://osu.ppy.sh" target="_blank">osu!</a>
 				</p>
 			</div>
 		</footer>

180f491a7cf88e432e89816558c61006343dd25d
David Collinson <hiroto@hiroto.eu>
2014-05-04 01:24:14 +0100

    Make the admin panel r/a/dio logo go to the main homepage

--- app/views/admin/navbar.blade.php
+++ app/views/admin/navbar.blade.php
@@ -11,7 +11,7 @@
 						<span class="icon-bar"></span>
 						<span class="icon-bar"></span>
 					</button>
-					<a class="navbar-brand" href="/admin"><img src="/assets/logotitle_2.png" height="22"></a>
+					<a class="navbar-brand" href="/"><img src="/assets/logotitle_2.png" height="22"></a>
 				</div>
 
 				<!-- Navbar Itself

fcf504be03ea2f47d44216d0c4524eca5366394f
David Collinson <hiroto@hiroto.eu>
2014-05-12 02:18:54 +0100

    Reverse database username change

--- app/config/database.php
+++ app/config/database.php
@@ -56,7 +56,7 @@ return array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
 			'database'  => 'radio',
-			'username'  => 'root',
+			'username'  => 'radio',
 			'password'  => '',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',

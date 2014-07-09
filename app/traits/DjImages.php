56c27017010b8457964b9a99c70e81253caae547
David Collinson <hiroto@hiroto.eu>
2014-07-09 18:29:04 +0100

    Fix formatting in DJImages (and also add a default DJ image)

--- app/traits/DjImages.php
+++ app/traits/DjImages.php
@@ -1,82 +1,62 @@
 <?php
 
 trait DjImages {
-        public function getDjImage($id)
-        {
-				// This entire function is a mess. It throws 500 left and right and
-				// I have no idea why.
-                $dj = Dj::findOrFail($id);
-				
-				$path = Config::get("radio.paths.dj-images") . "/" . $dj->djimage;
-				
-				if(!File::exists($path)) {
-					//TODO: have this return the blank dj image?
-					App::abort(404);
-					return;
-				}
-				
-                $resp = Response::make(
-					File::get($path),
-					200
-				);
-				
-				// Yes, I know it might be something else, but we don't know and honestly,
-				// browsers don't care either.
-				$resp->header(
-					'Content-type',
-					'image/png'
-				);
-				
-				return $resp;
-        }
+	public function getDjImage($id) {
+		$dj = Dj::findOrFail($id);
+		
+		$path = Config::get("radio.paths.dj-images") . "/" . $dj->djimage;
+		
+		if (! File::exists($path)) {
+			$resp = Response::make(File::get(public_path() . "/assets/dj-image.png"), 200);
+		} else {
+			$resp = Response::make(File::get($path), 200);
+		}
+		
+		// Yes, I know it might be something else, but we don't know and honestly,
+		// browsers don't care either.
+		$resp->header('Content-type', 'image/png');
+		
+		return $resp;
+	}
 
-        public function putDjImage($id)
-        {
-                if (! Auth::check())
-                        return;
+	public function putDjImage($id) {
+		if (! Auth::check())
+				return;
 
-                $dj = Dj::findOrFail($id);
-                $user = Auth::user();
+		$dj = Dj::findOrFail($id);
+		$user = Auth::user();
 
-                if (($dj->user->id === $user->id) or
-                        ($user->isDev()))
-                {
-                        $image = Input::file("image");
-                        $image->moveUploadedFile(Config::get("radio.paths.dj-images"), $dj->djimage);
-                }
-        }
+		if (($dj->user->id === $user->id) or ($user->isDev())) {
+			$image = Input::file("image");
+			$image->moveUploadedFile(Config::get("radio.paths.dj-images"), $dj->djimage);
+		}
+	}
 
-        public function postDjImage($id)
-        {
-                if (! Auth::check())
-                        return;
+	public function postDjImage($id) {
+		if (! Auth::check())
+			return;
 
-                $dj = Dj::findOrFail($id);
-                $user = Auth::user();
+		$dj = Dj::findOrFail($id);
+		$user = Auth::user();
 
-                if (($dj->user->id === $user->id) or
-                        ($user->isDev()))
-                {
-                        $image = Input::file("image");
-                        $image->moveUploadedFile(Config::get("radio.paths.dj-images"), $dj->id);
+		if (($dj->user->id === $user->id) or ($user->isDev())) {
+			$image = Input::file("image");
+			$image->moveUploadedFile(Config::get("radio.paths.dj-images"), $dj->id);
+		}
+	}
 
-                }
-        }
+	public function deleteDjImage($id)
+	{
+		if (! Auth::check())
+			return;
 
-        public function deleteDjImage($id)
-        {
-                if (! Auth::check())
-                        return;
+		$dj = Dj::findOrFail($id);
+		$user = Auth::user();
 
-                $dj = Dj::findOrFail($id);
-                $user = Auth::user();
-
-                if (($dj->user->id === $user->id) or
-                        ($user->isDev()))
-                {
-                        unlink(Config::get("radio.paths.dj-images", "") . "/" . $dj->djimage);
-                        $dj->image = null;
-                        $dj->save();
-                }
-        }
+		if (($dj->user->id === $user->id) or ($user->isDev())) {
+			unlink(Config::get("radio.paths.dj-images", "") . "/" . $dj->djimage);
+			$dj->image = null;
+			$dj->save();
+		}
+	}
 }

fb8bc5dd502fa0f97d1be4e61609206e57c4d0a4
Amelia Ikeda <hiroto@hiroto.eu>
2014-07-10 02:15:38 +0100

    Place player on the database viewer at the bottom of the page. Fixes #36

--- app/views/admin/database.blade.php
+++ app/views/admin/database.blade.php
@@ -1,73 +1,68 @@
 @section("content")
+	<div style="position: fixed; bottom: 0; z-index: 9999; background: rgba(0, 0, 0, 0.3); padding: 20px 30px 0 30px; width: 100%">
 
-	<div class="container main">
-		<div class="row">
-			<div class="row">
-				<div class="col-lg-8">
-					{{ Form::open(["url" => "/admin/songs/search"]) }}
-						<div class="input-group">
-							<input type="text" class="form-control" placeholder="{{{ trans("search.placeholder") }}}" name="q" id="search" value="{{{ $search }}}">
-							<div class="input-group-btn">
-								<button class="btn btn-info" type="submit">
-									{{{ trans("search.button") }}}
-								</button>
-							</div>
+		<div class="container">
+			<div id="player-test">
+				<div>
+					<div class="col-xs-1" style="padding-right: 0" id="play-pause">
+						<i class="fa fa-play audio-icon" id="audio-play"></i>
+						<i class="fa fa-pause audio-icon" id="audio-pause" style="display: none"></i>
+					</div>
+					<div class="col-xs-5" id="audio-slider">
+						<input id="audio-progress" type="range" class="track-slider" min="0" max="1000" step="1" value="0">
+						<div class="buffer" style="margin-right: 30px">
+							<div id="audio-buffer" class="buffer-bar" style="width: 0%"></div>
 						</div>
-					{{ Form::close() }}
-					@if (isset($time))
-						<br>
-						<p class="text-center text-info">Search took {{{ $time }}}ms</p>
-					@endif
-				</div>
-				<div class="col-lg-4">
-					<div class="row">
-						<div id="player-test">
-							<div>
-								<div class="col-xs-1" style="padding-right: 0" id="play-pause">
-									<i class="fa fa-play audio-icon" id="audio-play"></i>
-									<i class="fa fa-pause audio-icon" id="audio-pause" style="display: none"></i>
-								</div>
-								<div class="col-xs-5" id="audio-slider">
-									<input id="audio-progress" type="range" class="track-slider" min="0" max="1000" step="1" value="0">
-									<div class="buffer" style="margin-right: 30px">
-										<div id="audio-buffer" class="buffer-bar" style="width: 0%"></div>
-									</div>
-								</div>
-								<div class="col-xs-1" id="audio-time">
-									0:00
-								</div>
-								<div class="col-xs-1" style="padding-right: 0">
-									<i class="fa fa-volume-up audio-icon" id="volume-high"></i>
-									<i class="fa fa-volume-down audio-icon" id="volume-low" style="display: none"></i>
-									<i class="fa fa-volume-off audio-icon" id="volume-muted" style="color: rgb(153, 153, 153); display: none"></i>
-								</div>
-								<div class="col-xs-2" style="padding-right: 0">
-									<input type="range" class="track-slider" min="0" max="100" step="1" value="80" id="volume">
-									<div class="buffer" style="margin-right: 15px">
-										<div id="audio-bar" class="buffer-bar" style="width: 80%"></div>
-									</div>
-									
-								</div>
-								<div class="col-xs-2">
-									<button class="btn btn-xs btn-default" style="margin-top: 2px;" id="audio-reset">reset</button>
-								</div>
-							</div>
+					</div>
+					<div class="col-xs-1" id="audio-time">
+						0:00
+					</div>
+					<div class="col-xs-1" style="padding-right: 0">
+						<i class="fa fa-volume-up audio-icon" id="volume-high"></i>
+						<i class="fa fa-volume-down audio-icon" id="volume-low" style="display: none"></i>
+						<i class="fa fa-volume-off audio-icon" id="volume-muted" style="color: rgb(153, 153, 153); display: none"></i>
+					</div>
+					<div class="col-xs-2" style="padding-right: 0">
+						<input type="range" class="track-slider" min="0" max="100" step="1" value="80" id="volume">
+						<div class="buffer" style="margin-right: 15px">
+							<div id="audio-bar" class="buffer-bar" style="width: 80%"></div>
 						</div>
-						<p id="np" class="text-center"></p>
+						
+					</div>
+					<div class="col-xs-2">
+						<button class="btn btn-xs btn-danger" style="margin-top: 2px;" id="audio-reset">debug</button>
 					</div>
 				</div>
 			</div>
+			<p id="np" class="text-center" style="color: #fff"></p>
+		</div>
+	</div>
 
-			<hr>
+	<div class="container main">
+		<div class="row col-lg-8 text-center col-lg-offset-2">
+			{{ Form::open(["url" => "/admin/songs/search"]) }}
+				<div class="input-group">
+					<input type="text" class="form-control" placeholder="{{{ trans("search.placeholder") }}}" name="q" id="search" value="{{{ $search }}}">
+					<div class="input-group-btn">
+						<button class="btn btn-info" type="submit">
+							{{{ trans("search.button") }}}
+						</button>
+					</div>
+				</div>
+			{{ Form::close() }}
+			@if (isset($time))
+				<br>
+				<p class="text-center text-info">Search took {{{ $time }}}ms</p>
+			@endif
 		</div>
 	</div>
 
 	@foreach ($results as $result)
-                @if ($result["_source"]["usable"] == 1)
-                <div class="well" style="padding-bottom: 0; padding-top: 8px">
-                @else
-                <div class="well" style="background-color: rgba(217, 83, 79, 0.5); padding-bottom: 0; padding-top: 8px">
-                @endif
+        @if ($result["_source"]["usable"] == 1)
+        <div class="well" style="padding-bottom: 0; padding-top: 8px">
+        @else
+        <div class="well" style="background-color: rgba(217, 83, 79, 0.5); padding-bottom: 0; padding-top: 8px">
+        @endif
 			{{ Form::open(["url" => "/admin/songs/" . $result["_id"], "class" => "form-horizontal"]) }}
 				<div class="container" style="width: 100%; padding: 0">
 					<div class="col-lg-3">

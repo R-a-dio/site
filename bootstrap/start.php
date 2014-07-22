70e4311ae8ba21e40f675896be2e3f7b848a5b5b
Amelia Ikeda <hiroto@hiroto.eu>
2014-07-22 18:56:07 +0100

    Add machine names and environments to disable debug mode in

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -26,7 +26,8 @@ $app = new Illuminate\Foundation\Application;
 
 $env = $app->detectEnvironment(array(
 
-	'local' => array('your-machine-name'),
+	'local' => ['testing', 'radio'],
+	'production' => ['dev.hiroto.eu'],
 
 ));
 

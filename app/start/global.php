283c3701df2f52fcca5c8968e893372399757641
Amelia Ikeda <amelia@amelia.pw>
2014-09-19 23:33:53 +0100

    Add controllers to the autoloader

--- app/start/global.php
+++ app/start/global.php
@@ -14,13 +14,13 @@ use Illuminate\Database\Eloquent\ModelNotFoundException;
 */
 
 ClassLoader::addDirectories(array(
-
 	app_path().'/commands',
-	app_path().'/controllers',
+	app_path().'/controllers/admin',
+	app_path().'/controllers/site',
+	app_path().'/controllers/api',
 	app_path().'/models',
 	app_path().'/database/seeds',
 	app_path().'/traits',
-
 ));
 
 /*

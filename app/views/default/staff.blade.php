192874bfb0056258bff2ebe7db643da214625ae2
tamamoe <tamamoe@gmx.com>
2014-09-28 10:24:15 +0300

    staff page changes

--- app/views/default/staff.blade.php
+++ app/views/default/staff.blade.php
@@ -1,5 +1,58 @@
 @section("content")
 	<div class="container main content">
+	
+	<div class="staffpage-title col-md-12"><h3>Staff</h3></div>
+	<div class="panel-default djpage-panel col-md-12 col-sm-12 col-xs-12">
+	@foreach($staff as $i => $s)
+		@if ($s["role"] == '')
+			<div class="col-md-3 col-sm-4 col-xs-6">
+				<div class="thumbnail">
+					<br>
+					<img src="/api/dj-image/{{ $s["id"] }}" alt="{{{ $s["djname"] }}}" height="150" width="150" style="max-height: 150px">
+					<div class="text-center staff-name">
+						<h4>{{{ $s["djname"] }}}</h4>
+					</div>
+				</div>
+			</div>
+		@endif
+	@endforeach
+	</div>
+	
+	<div class="staffpage-title col-md-12"><h3>Developers</h3></div>
+	<div class="panel-default djpage-panel col-md-12 col-sm-12 col-xs-12">
+	@foreach($staff as $i => $s)
+		@if ($s["role"] == "dev")
+			<div class="col-md-3 col-sm-4 col-xs-6">
+				<div class="thumbnail">
+					<br>
+					<img src="/api/dj-image/{{ $s["id"] }}" alt="{{{ $s["djname"] }}}" height="150" width="150" style="max-height: 150px">
+					<div class="text-center staff-name">
+						<h4>{{{ $s["djname"] }}}</h4>
+					</div>
+				</div>
+			</div>
+		@endif
+	@endforeach
+	</div>
+	
+	<div class="staffpage-title col-md-12"><h3>DJs</h3></div>
+	<div class="panel-default djpage-panel col-md-12 col-sm-12 col-xs-12">
+	@foreach($staff as $i => $s)
+		@if ($s["role"] == "dj")
+			<div class="col-md-3 col-sm-4 col-xs-6">
+				<div class="thumbnail">
+					<br>
+					<img src="/api/dj-image/{{ $s["id"] }}" alt="{{{ $s["djname"] }}}" height="150" width="150" style="max-height: 150px">
+					<div class="text-center staff-name">
+						<h4>{{{ $s["djname"] }}}</h4>
+					</div>
+				</div>
+			</div>
+		@endif
+	@endforeach
+	</div>
+	
+<!--
 	@foreach($staff as $i => $s)
 
 		<div class="col-md-3">
@@ -28,5 +81,6 @@
 		</div>
 
 	@endforeach
+-->
 	</div>
 @stop

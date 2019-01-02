afd4bc0881f9d5de4a05b23c7bb77be00cc176c7
Vin <vin@r-a-d.io>
2019-01-02 17:22:10 +0000

    Connect securely with web irc

--- app/views/default/irc.blade.php
+++ app/views/default/irc.blade.php
@@ -1,6 +1,6 @@
 @section('content')
 	<div class="container main content">
-		<iframe src="https://kiwiirc.com/client/irc.rizon.net/?theme=cli#r/a/dio"
+		<iframe src="https://kiwiirc.com/client/irc.rizon.net:+6697/?theme=cli#r/a/dio"
 			style="border:0; width:100%; height:700px;"
 		></iframe>
 	</div>

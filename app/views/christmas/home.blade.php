3e9e86ddcb234186c6f24a93f69ae10f0a4dab4a
Vin <vin@r-a-d.io>
2019-01-02 17:21:02 +0000

    Fix christmas and NYE themes
    Show LP page on NYE theme
    Do something to christmas theme I dunno
    
    Why is that stuff inline in the blade?

--- app/views/christmas/home.blade.php
+++ app/views/christmas/home.blade.php
@@ -64,6 +64,22 @@
 </div><!-- /.container.content -->
 </div><!-- /.container.main -->
 </div>
+<style>
+	#dj-mode h2 {
+		background: rgba(24,32,36,0.4);
+		background: radial-gradient(at bottom,
+			rgba(24,32,36,0.4),
+			rgba(24,32,36,0) 60%
+		);
+		box-shadow: 0 .2em .2em -.2em hsla(212, 60%, 20%, 0.3);
+	}
+	.lp-meta.text-center, .listeners-num, .timer-num {
+		text-shadow: .03em .03em .01em #012;
+	}
+	#np, #dj-mode h2, #dj-mode h1 a {
+		text-shadow: .02em .02em .04em #012;
+	}
+</style>
 @stop
 @section('script')
 @stop

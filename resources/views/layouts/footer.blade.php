50e437612269c90bfff0ae4b22debf08380e174b
Vin <vin@r-a-d.io>
2020-02-18 21:46:41 +0000

    Parity change with develop for a917fff3b8b8c0ba8830ef569daff20978b8eb0e.

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,11 +1,9 @@
                 <footer>
                         <div class="footer">
                                 <p class="text-muted text-center">
-					<a class="text-muted" href="https://play.google.com/store/apps/details?id=io.r_a_d.radio">Android</a>
+										<a class="text-muted" href="https://play.google.com/store/apps/details?id=io.r_a_d.radio" target="_blank">Android</a>
                                         |
-					<a class="text-muted" href="https://itunes.apple.com/us/app/r-a-dio/id1207600789">iOS</a>
-                                        |
-                                        <a class="text-muted" href="https://twitter.com/Hanyuu_status">Twitter</a>
+                                        <a class="text-muted" href="https://twitter.com/Hanyuu_status" target="_blank">Twitter</a>
                                         |
                                         <a class="text-muted" href="https://github.com/R-a-dio/site" target="_blank">Github</a>
                                 </p>

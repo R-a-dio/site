35dc6115ec7a5dd901f0bb6b2e574e381594cf36
Vin <vin@r-a-d.io>
2020-05-19 19:37:52 +0000

    Add IRC server and channel to the IRC page. #106

--- app/views/default/irc.blade.php
+++ app/views/default/irc.blade.php
@@ -1,5 +1,8 @@
 @section('content')
 	<div class="container main content">
+		<div class="well row">
+			You can either use the below webchat, or connect directly to <a href="irc://irc.rizon.net/%23r%2Fa%2Fdio/">#r/a/dio on irc.rizon.net</a>.
+		</div>
 		<iframe src="https://kiwiirc.com/client/irc.rizon.net:+6697/?theme=cli#r/a/dio"
 			style="border:0; width:100%; height:700px;"
 		></iframe>

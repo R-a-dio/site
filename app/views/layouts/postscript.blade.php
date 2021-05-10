11fbb904d6575f3dba7560fc369dd78b87608e72
john smith <realjohnsmithkapp@gmail.com>
2021-05-10 16:31:58 +0300

    display song tags when currently playing song is clicked

--- app/views/layouts/postscript.blade.php
+++ app/views/layouts/postscript.blade.php
@@ -648,6 +648,7 @@
 
 	var ravespeed = 2;
 	var ravemulti = 1;
+	
 	if (localStorage.getItem("isRave") === "true" ) {
 		ravemode();
 	}
@@ -703,3 +704,51 @@
 	}
 </script>
 <!-- rave end -->
+<!-- tags script start -->
+<script>
+if (localStorage.getItem("displayTags") === null) {
+    localStorage.setItem("displayTags", "false");
+}
+
+// add the tag part under currently playing song after checking it's display status
+switch (localStorage.getItem("displayTags")) {
+    case "false":
+        document.getElementById("np").insertAdjacentHTML('afterend', '<p id="tags" style="font-size:14px;display:none;"></p>');
+        break;
+    case "true":
+        document.getElementById("np").insertAdjacentHTML('afterend', '<p id="tags" style="font-size:14px;"></p>');
+        fetchAndDisplayTags();
+        break;
+}
+
+// set up and use mutationobserver to observe changes to currently playing song
+const observeTarget = document.querySelector("#np");
+const config = { attributes: true, childList: true, subtree: true };
+const observer = new MutationObserver(fetchAndDisplayTags)
+observer.observe(observeTarget, config);
+
+// fetch new tags and insert them into the dom
+function fetchAndDisplayTags() {
+    fetch('https://r-a-d.io/api')
+        .then(response => response.json())
+        .then(data => data["main"]["tags"])
+        .then(tags => {
+            document.getElementById("tags").innerHTML = "tags: " + tags.join(" ");
+        });
+}
+
+// add eventlistener to toggle display of tags when song title is clicked
+document.getElementById("np").addEventListener("click", function() {
+    switch (localStorage.getItem("displayTags")) {
+        case "false":
+            localStorage.setItem("displayTags", "true");
+            document.getElementById("tags").style.display = "inherit";
+            break;
+        case "true":
+            localStorage.setItem("displayTags", "false");
+            document.getElementById("tags").style.display = "none";
+            break;
+    }
+});
+</script>
+<!-- tags script end -->

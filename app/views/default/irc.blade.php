0557f99b1329d61e6d82ef1cc7954747e924f720
Vin <vin@r-a-d.io>
2023-08-10 17:46:39 +0000

    Update text on irc page and add newer (working) irc webclient.

--- app/views/default/irc.blade.php
+++ app/views/default/irc.blade.php
@@ -1,9 +1,10 @@
 @section('content')
 	<div class="container main content">
 		<div class="well row">
-			You can either use the below webchat, or connect directly to <a href="irc://irc.rizon.net/%23r%2Fa%2Fdio/">#r/a/dio on irc.rizon.net</a>.
+			<p>You can either use the below webchat, or connect directly to <a href="irc://irc.rizon.net/%23r%2Fa%2Fdio/">#r/a/dio on irc.rizon.net</a>.</p>
+			<p>If you're here for the daypass and exci isn't responding, you can ask anyone else with <b>%</b>, <b>@</b> or <b>&</b> before their nick - just tell them exci is AFK, as well as what and how much you're going to upload.</p>
 		</div>
-		<iframe src="https://kiwiirc.com/client/irc.rizon.net:+6697/?theme=cli#r/a/dio"
+		<iframe src="https://kiwiirc.com/nextclient/#irc://irc.rizon.net:+6697/#r/a/dio"
 			style="border:0; width:100%; height:700px;"
 		></iframe>
 	</div>

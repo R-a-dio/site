6095143758bac05de6142f88952e4f4f5fffa324
Vin <vin@r-a-d.io>
2023-08-10 17:49:26 +0000

    Update submission guidelines and add a partial with a dialog.

---
+++ resources/views/partials/submission-guidelines.blade.php
@@ -0,0 +1,38 @@
+<div class="modal fade bs-modal-lg" id="submission-guidelines">
+	<div class="modal-dialog modal-lg">
+		<div class="modal-content">
+			<div class="modal-header">
+				<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+				<h4 class="modal-title">Song submission guidelines</h4>
+			</div>
+			<div class="modal-body">
+				<h3>Your submission got declined because of one or more of the following:</h3>
+				<ul>
+					<li>It wasn't from a Japanese anime, game, or other media production.</li>
+					<li>Nobody involved in the production of the song was from Japan.</li>
+					<li>It was TV size; we only accept full size releases.</li>
+					<li>It was low quality and/or a youtube/soundcloud/other bad source rip. Note that bitrate is not always indicative of quality. Anything below 192kbps is rejected either way.</li>
+					<li>It was already in the database. Sometimes songs are mistagged, and that's our fault. You should still search as thoroughly as possible to make sure what you're uploading isn't a duplicate</li>
+					<li>Someone had uploaded the same song, but it wasn't accepted yet, in which case the submission that's highest quality gets accepted and the others declined.</li>
+				</ul>
+				<h3>Your submission <u>might</u> have gotten declined because of one or more of the following:</h3>
+				<ul>
+					<li>It had a bitrate below at least 256kbps.</li>
+					<li>It was a bad transcode (for example, from 192kbps mp3 to 320kbps mp3).</li>
+					<li>It's too niche (nobody wants to listen to it except you).</li>
+					<li>If tags are in Japanese: you didn't mention source, artist, song title, etc.</li>
+				</ul>
+				<h3>"But there are songs in the database that don't follow these rules!"</h3>
+					<p>Yes, and those won't be removed. They were accepted when there were not really any rules for accepting submissions. We just won't accept stuff like that anymore.</p>
+				<h3>So what can I upload?</h3>
+					<p>Any high quality music that's /a/ or /jp/ related, including doujin music, remixes, mashups, video game music, jpop, etc. <u>Simply having Japanese influence doesn't make something /a/ or /jp/</u>, and any such submissions will be declined. 
+					<p><b>Remixes and mashups are exempt from the "nobody involved in production is from Japan" rule</b>, and are ok to upload, as long as the original song that was remixed or mashed up would have been accepted. There's a little more scrutiny with these, however; bad mashups or remixes might get declined even if otherwise acceptable.</p>
+			</div>
+
+
+			<div class="modal-footer">
+				<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+			</div>
+		</div><!-- /.modal-content -->
+	</div><!-- /.modal-dialog -->
+</div><!-- /.modal -->
